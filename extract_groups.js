cbc33e867341d8013b7b45cd3eb2baf75472f8ea
Migrabe <migrabe@users.noreply.github.com>
2026-03-05 11:37:44 +0200

    Wipe repository: user requested full cleanup

--- extract_groups.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-const html = fs.readFileSync('public/index.html', 'utf8');
-const regex = /data-group="([^"]+)"/g;
-let match;
-const groups = new Set();
-while ((match = regex.exec(html)) !== null) {
-    groups.add(match[1]);
-}
-console.log(Array.from(groups).sort());
